2277f9e3dfd6a0d0e946423bd9e5e1ac79547339
Alexey Valitov <alexeyvalitov28@gmail.com>
2023-01-17 01:49:25 +0500

    final message

--- tg-bot/cmd/main-inline-buttons.go
+++ tg-bot/cmd/main-inline-buttons.go
@@ -6,27 +6,24 @@ import (
 	"log"
 )
 
+const pinMsg = "Поблагодарить автора бота 😎"
+const pinButton = "СДЕЛАТЬ ПО КАЙФУ"
+const pinAnswer = "Поддержать проект: `4276 3802 1719 2553`"
+
 var numericKeyboard = tgbotapi.NewInlineKeyboardMarkup(
 	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonURL("1.com", "http://1.com"),
-		tgbotapi.NewInlineKeyboardButtonData("2", "2"),
-		tgbotapi.NewInlineKeyboardButtonData("3", "3"),
-	),
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("4", "4"),
-		tgbotapi.NewInlineKeyboardButtonData("5", "5"),
-		tgbotapi.NewInlineKeyboardButtonData("6", "6"),
+		tgbotapi.NewInlineKeyboardButtonData(pinButton, pinAnswer),
 	),
 )
 
 func main() {
 	// подключаемся к боту с помощью токена
-	bot, err := tgbotapi.NewBotAPI("TOKEN")
+	bot, err := tgbotapi.NewBotAPI("5941964544:AAGonU-msKpkY9N-W4E0CblToGSA9bW8SWI")
 	if err != nil {
 		log.Panic(err)
 	}
 
-	bot.Debug = true
+	//bot.Debug = true
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
@@ -39,34 +36,31 @@ func main() {
 	for update := range updates {
 		// Check if we've gotten a message update.
 		if update.Message != nil {
-			// Construct a new message from the given chat ID and containing
-			// the text that we received.
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-
 			// If the message was open, add a copy of our numeric keyboard.
 			switch update.Message.Text {
 			case "open":
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, pinMsg)
 				msg.ReplyMarkup = numericKeyboard
 
-			}
-
-			// Send the message.
-			if _, err = bot.Send(msg); err != nil {
-				fmt.Println(err)
-				return
+				// Send the message.
+				if _, err = bot.Send(msg); err != nil {
+					fmt.Println(err)
+					return
+				}
 			}
 		} else if update.CallbackQuery != nil {
 			// Respond to the callback query, telling Telegram to show the user
 			// a message with the data received.
 			callback := tgbotapi.NewCallback(update.CallbackQuery.ID, update.CallbackQuery.Data)
-			if _, err := bot.AnswerCallbackQuery(callback); err != nil {
+			if _, err = bot.AnswerCallbackQuery(callback); err != nil {
 				fmt.Println(err)
 				return
 			}
 
 			// And finally, send a message containing the data received.
 			msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Data)
-			if _, err := bot.Send(msg); err != nil {
+			msg.ParseMode = tgbotapi.ModeMarkdown
+			if _, err = bot.Send(msg); err != nil {
 				fmt.Println(err)
 				return
 			}
