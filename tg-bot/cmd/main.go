5f54efce9eccdb987141c7cfb5d8869cc000a300
Alexey Valitov <alexeyvalitov28@gmail.com>
2023-01-16 19:04:56 +0500

    v1

--- tg-bot/cmd/main.go
+++ tg-bot/cmd/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"fmt"
 	tgbotapi "github.com/Syfaro/telegram-bot-api"
 	"log"
-	"reflect"
+	"strconv"
+	"strings"
 )
+
+var count = 2
+var maxPages = len(data) / count // = 5
+
+var data = []string{"DummyData1", "DummyData2", "DummyData3", "DummyData4", "DummyData5", "DummyData6", "DummyData7", "DummyData8", "DummyData9", "DummyData10"}
 
 func main() {
 	// подключаемся к боту с помощью токена
@@ -19,6 +26,10 @@ func main() {
 	var ucfg = tgbotapi.NewUpdate(0)
 	ucfg.Timeout = 60
 	upd, _ := bot.GetUpdatesChan(ucfg)
+
+	var chatId = int64(746200068) // <--- Place Chat Id Here
+
+	SendDummyData(bot, chatId, 0, 2, nil) // Send initial data
 
 	// читаем обновления из канала
 	for {
@@ -42,7 +53,8 @@ func main() {
 
 			log.Printf("[%s, %s] %d %s", UserName, UserNameFirstName, ChatID, Text)
 
-			if reflect.TypeOf(update.Message.Text).Kind() == reflect.String && update.Message.Text != "" {
+			/*if reflect.TypeOf(update.Message.Text).Kind() == reflect.String && update.Message.Text != "" */
+			{
 				switch {
 				case Text == "/start":
 					msgText := "Привет, "
@@ -57,8 +69,70 @@ func main() {
 						ChatID,
 						msgText)
 					_, _ = bot.Send(msg)
+				case update.CallbackQuery != nil:
+					CallbackQueryHandler(bot, update.CallbackQuery, ChatID)
+					continue
 				}
 			}
 		}
 	}
+}
+
+func CallbackQueryHandler(bot *tgbotapi.BotAPI, query *tgbotapi.CallbackQuery, ChatID int64) {
+	split := strings.Split(query.Data, ":")
+	if split[0] == "pager" {
+		HandleNavigationCallbackQuery(bot, query.Message.MessageID, ChatID, split[1:]...)
+		return
+	}
+}
+
+func HandleNavigationCallbackQuery(bot *tgbotapi.BotAPI, messageId int, ChatID int64, data ...string) {
+	pagerType := data[0]
+	currentPage, _ := strconv.Atoi(data[1])
+	itemsPerPage, _ := strconv.Atoi(data[2])
+
+	if pagerType == "next" {
+		nextPage := currentPage + 1
+		if nextPage < maxPages {
+			SendDummyData(bot, ChatID, nextPage, itemsPerPage, &messageId)
+		}
+	}
+	if pagerType == "prev" {
+		previousPage := currentPage - 1
+		if previousPage >= 0 {
+			SendDummyData(bot, ChatID, previousPage, itemsPerPage, &messageId)
+		}
+	}
+}
+
+func SendDummyData(bot *tgbotapi.BotAPI, chatId int64, currentPage, count int, messageId *int) {
+	text, keyboard := DummyDataTextMarkup(currentPage, count)
+
+	var cfg tgbotapi.Chattable
+	if messageId == nil {
+		msg := tgbotapi.NewMessage(chatId, text)
+		msg.ReplyMarkup = keyboard
+		cfg = msg
+	} else {
+		msg := tgbotapi.NewEditMessageText(chatId, *messageId, text)
+		msg.ReplyMarkup = &keyboard
+		cfg = msg
+	}
+
+	bot.Send(cfg)
+}
+
+func DummyDataTextMarkup(currentPage, count int) (text string, markup tgbotapi.InlineKeyboardMarkup) {
+	text = strings.Join(data[currentPage*count:currentPage*count+count], "\n")
+
+	var rows []tgbotapi.InlineKeyboardButton
+	if currentPage > 0 {
+		rows = append(rows, tgbotapi.NewInlineKeyboardButtonData("Previous", fmt.Sprintf("pager:prev:%d:%d", currentPage, count)))
+	}
+	if currentPage < maxPages-1 {
+		rows = append(rows, tgbotapi.NewInlineKeyboardButtonData("Next", fmt.Sprintf("pager:next:%d:%d", currentPage, count)))
+	}
+
+	markup = tgbotapi.NewInlineKeyboardMarkup(rows)
+	return
 }
